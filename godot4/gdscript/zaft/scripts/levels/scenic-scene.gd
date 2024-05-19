dcf5d9300650494e95f92fd3bb04632a52296bae
zaftnotameni <mozair.carmo+zaft@gmail.com>
2024-05-19 09:37:11 +0200

    deactivation zones

--- godot4/gdscript/zaft/scripts/levels/scenic-scene.gd
+++ godot4/gdscript/zaft/scripts/levels/scenic-scene.gd
@@ -3,6 +3,8 @@ class_name ScenicScene extends Node2D
 @onready var polygons : Node2D = $Polygons
 @onready var bodies := PolygonColliderSetupNode.new()
 @onready var activation_zone : Area2D = $WallActivationZone
+@onready var deactivation_zone_right : Area2D = $WallDeactivationZoneRight
+@onready var deactivation_zone_left : Area2D = $WallDeactivationZoneLeft
 
 var wall_left_body : LoopyStaticBody2D
 var wall_right_body : LoopyStaticBody2D
@@ -23,13 +25,20 @@ func on_body_entered_activation_zone(_p:PlayerCharacterScene):
   enable_left_wall()
   enable_right_wall()
 
+func on_body_entered_deactivation_zone(_p:PlayerCharacterScene, b:LoopyStaticBody2D):
+  b.disable_loop_collision()
+
 func enable_left_wall(): if wall_left_body != null: wall_left_body.enable_loop_collision()
 func enable_right_wall(): if wall_right_body != null: wall_right_body.enable_loop_collision()
 func disable_left_wall(): if wall_left_body != null: wall_left_body.disable_loop_collision()
 func disable_right_wall(): if wall_right_body != null: wall_right_body.disable_loop_collision()
 
-func _ready() -> void:
+func setup_zone_signals():
   activation_zone.body_entered.connect(on_body_entered_activation_zone)
+  deactivation_zone_right.body_entered.connect(on_body_entered_deactivation_zone.bind(wall_right_body))
+  deactivation_zone_left.body_entered.connect(on_body_entered_deactivation_zone.bind(wall_left_body))
+
+func _ready() -> void:
   add_child(bodies)
   bodies.copy_polygons_to_bodies(polygons)
   __util.read_bodies_and_polies(self, bodies, polygons)
@@ -39,3 +48,4 @@ func _ready() -> void:
   ground_2_body.loop_collision_can_be_disabled = false
   wall_right_2_body.loop_collision_can_be_disabled = false
   ceiling_body.loop_collision_can_be_disabled = false
+  setup_zone_signals()
