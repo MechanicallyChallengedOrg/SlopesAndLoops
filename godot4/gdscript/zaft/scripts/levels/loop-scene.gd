3c679d3ca47a532aa94994001a964edafdce91ae
zaftnotameni <mozair.carmo+zaft@gmail.com>
2024-05-19 11:44:25 +0200

    adding comments to important places

--- godot4/gdscript/zaft/scripts/levels/loop-scene.gd
+++ godot4/gdscript/zaft/scripts/levels/loop-scene.gd
@@ -2,19 +2,42 @@ class_name LoopScene extends Node2D
 
 @onready var polygons : Node2D = $Polygons
 @onready var bodies := PolygonColliderSetupNode.new()
+
+# Activation Zone:
+# When the player passes through the middle of the loop, no matter from which direction,
+# the character will go through this area and activate both walls of the loop.
+# This turns the loop into a prison of sorts, and the player can only go out in 2 ways:
+# - A) Jumping on the slope of the loop, which just lets the player fall through and then go out
+# - B) Going around the loop at enough speed so the loop can be completed, which lets the player out on the other side
 @onready var activation_zone : Area2D = $WallActivationZone
+
+# Deactivation Zones:
+# Because the implementation includes a solid wall around the loop,
+# these zones are added to deactivate that collision as the player passes by,
+# so the player can enter the loop. This could also be achieved with one way
+# collisions, but I wanted to demonstrate a different approach, that might be more
+# flexible in some situations.
 @onready var deactivation_zone_right : Area2D = $WallDeactivationZoneRight
 @onready var deactivation_zone_left : Area2D = $WallDeactivationZoneLeft
 
+# Wall/Floor/Ground Polygons:
+# These are just polygons drawn on the screen for the sake of
+# having some easy to make terrain for the demo.
+# Polygons don't have physics and you cannot add colliders to them,
+# for the demo I just wrote a simple script that creates a matching StaticBody
+# with a Polygon collision shape using the same vertex data as the visual polygon
+var wall_left_poly : Polygon2D
+var wall_right_poly : Polygon2D
+var ground_poly : Polygon2D
+var ceiling_poly : Polygon2D
+
+# Loopy Static Bodies
+# These are automatically generated based on the polygons above (the polygons are drawn in the editor)
 var wall_left_body : LoopyStaticBody2D
 var wall_right_body : LoopyStaticBody2D
 var ground_body : LoopyStaticBody2D
 var ceiling_body : LoopyStaticBody2D
 
-var wall_left_poly : Polygon2D
-var wall_right_poly : Polygon2D
-var ground_poly : Polygon2D
-var ceiling_poly : Polygon2D
 
 # specifying the type of _p here to provoke a crash if layers/masks are wrong
 func on_body_entered_activation_zone(_p:PlayerCharacterScene):
--- godot4/gdscript/zaft/scripts/loopy-static-body-node.gd
+++ godot4/gdscript/zaft/scripts/loopy-static-body-node.gd
@@ -1,6 +1,16 @@
 class_name LoopyStaticBody2D extends StaticBody2D
 
+# Matching polygon associated with this body,
+# the polygon is the visual representation, the body is the physics logic
+# in a proper game you'd probably either have a whole setup for terrain or
+# use tilesets.
+# If you are thinking in a tiles metaphor, the "poly" here is the tile,
+# while this class (the body) is the physics layer collider setup for that tile
 @export var poly : Polygon2D
+
+# Enables/Disables the "player can go through when jumping on a slope" functionality
+# on regular slopes you want to turn this off, while for the walls of a loop, you want to turn this on
+# this is setup incorrectly on purpose in the prison demo level
 @export var loop_collision_can_be_disabled := true
 
 func enable_loop_collision():
@@ -13,6 +23,8 @@ func disable_loop_collision() -> bool:
   set_alpha_multiplier_parameter_on_polygon(__config.ALPHA_MULTIPLIER_LOOP_OFF)
   return true
 
+# I'm using a shader for the polygons, that includes an alpha control
+# This is purely to illustrate when the collision for walls of the loop is on/off
 func set_alpha_multiplier_parameter_on_polygon(alpha:float=1.0, p:Polygon2D=poly):
   if p == null: return
   var m := p.material as ShaderMaterial
